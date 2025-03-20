231f143f7affce187c1329d5659b28fd00303f82
Lukasz Rakowski <raq1024@gmail.com>
2025-03-20 21:19:59 +0100

    small fix

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@
 #echo "permit nopass Administrator as root" | doas tee -a /usr/local/etc/doas.conf
 
 # register device on MQTT broker lobby
-doas cp mqtt-lobby.xml /usr/local/etc/TwinCAT/3.1/Target/Routes
+doas cp mqtt-lobby.xml /usr/local/etc/TwinCAT/3.1/Target/Routes/
