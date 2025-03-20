e0ece7da48db67db841fd61d32bfe84ff1eed5ac
Lukasz Rakowski <raq1024@gmail.com>
2025-03-20 22:20:26 +0100

    configs + setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,30 @@
 
 #echo "permit nopass Administrator as root" | doas tee -a /usr/local/etc/doas.conf
 
-# register device on MQTT broker lobby
-doas mkdir /usr/local/etc/TwinCAT/3.1/Target/Routes
-doas cp mqtt-lobby.xml /usr/local/etc/TwinCAT/3.1/Target/Routes/
+#check if the Routes directory already exists
+DIR="/usr/local/etc/TwinCAT/3.1/Target/Routes"
+if [ ! -d "$DIR" ]; then
+        mkdir -p "$DIR"
+        #register device on MQTT broker lobby
+        cp /configs/mqtt-lobby.xml /"$DIR"/
+        echo "current config :"
+        echo "lobby"
+else
+        echo "select configuration :
+        prod
+        test
+        dev"
+
+        read conf
+
+        param=$1
+
+        echo "selected :"
+        echo "mqtt-"${conf}".xml"
+
+        rm "$DIR"/*
+        cp configs/mqtt-${conf}.xml "$DIR"/
+fi
+
+#restart TwinCAT Router
+doas service TcSystemService restart
